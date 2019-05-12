4a6a1c6c2827bc2430883101d4931c5232fa1884
Anis DA SILVA CAMPOS <yocorporation@hotmail.com>
2019-05-12 10:30:38 -0400

    making sure browser closes

--- server/routes/annonces.js
+++ server/routes/annonces.js
@@ -4,13 +4,18 @@ const {Router} = require('express');
 const router = Router();
 
 router.post('/annonces', async (req, res) => {
+    let browser;
+    try {
+        browser = await req.context.puppeteer.launch({headless: false});
+        const leBonCoinScrapper = new LeBonCoinScrapper(browser);
+        await leBonCoinScrapper.load();
+        const data = await leBonCoinScrapper.scrap(1, 16);
+        return res.send({number: data.length});
+    } catch (e) {
+        next(e)
+    }
+    browser.close();
 
-    const browser = await req.context.puppeteer.launch({headless: false});
-    const leBonCoinScrapper = new LeBonCoinScrapper(browser);
-    await leBonCoinScrapper.load();
-    const data = await leBonCoinScrapper.scrap(1, 10);
-    browser.close()
-    return res.send({number: data.length});
 });
 
 router.get('/annonces', async (req, res) => {
